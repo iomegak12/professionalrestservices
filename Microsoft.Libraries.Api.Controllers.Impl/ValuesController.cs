83dd280c20ff52abea4f24c6e9b5440fa5e07caa
iomegak12 <jd.ramkumar@gmail.com>
2020-01-16 10:20:06 +0530

    Update ValuesController.cs

--- Microsoft.Libraries.Api.Controllers.Impl/ValuesController.cs
+++ Microsoft.Libraries.Api.Controllers.Impl/ValuesController.cs
@@ -3,13 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Authorization;
 
 
 namespace ProductServicesHosting.Controllers
 {
     [Route("api/[controller]")]
-    [Authorize]
     public class ValuesController : Controller
     {
         // GET api/values
